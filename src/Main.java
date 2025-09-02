8a3f362f17760b3effacb0d5e3a8e927f04a7d80
Victor Americo <victoroeste12@gmail.com>
2025-09-02 12:14:38 -0300

    iniciando a classe main, onde vai ser a principal, vou chamar todas as outras classes com essa

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+import entities.*;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scan.nextLine();
+        System.out.println("Digite seu email: ");
+        String email = scan.nextLine();
+        System.out.println("Digite sua data de aniversário");
+        String data = scan.nextLine();
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataConvertida = null;
+        try {
+            dataConvertida = formato.parse(data);
+        } catch (ParseException e) {
+            System.out.println("Data inválida!");
+        }
 
+        
 
         }// essa chave fecha a main
     } // essa chave fecha a classe
