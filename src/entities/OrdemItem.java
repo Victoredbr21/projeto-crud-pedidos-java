aba904e258fad39699e336bd1d976072c9995598
Victor Americo <victoroeste12@gmail.com>
2025-09-02 11:55:25 -0300

    criação da classe ordemitem

---
+++ src/entities/OrdemItem.java
@@ -0,0 +1,41 @@
+package entities;
+
+ public class OrdemItem {
+        private Integer quantity;
+        private Double price;
+
+        //construtores
+        public OrdemItem() {
+        } //sobrecarga
+
+        public OrdemItem(Integer quantity, Double price, Product Product) {
+            this.quantity = quantity;
+            this.price = price;
+
+        }
+
+        //getters
+
+        public Integer getQuantity() {
+            return quantity;
+        }
+
+        public Double getPrice() {
+            return price;
+        }
+
+        //setters
+
+        public void setQuantity(Integer quantity) {
+            this.quantity = quantity;
+        }
+
+        public void setPrice(Double price) {
+            this.price = price;
+        }
+        public Double subTotal() {
+            Double resultado = this.quantity * this.price;
+            return resultado;
+        }
+
+    } // essa chave fecha a classe
