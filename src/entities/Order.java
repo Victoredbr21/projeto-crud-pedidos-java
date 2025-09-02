dbaf437727eb003b389b1a3f9d4cc69b08b31f47
Victor Americo <victoroeste12@gmail.com>
2025-09-02 12:06:08 -0300

    criação da classe order

---
+++ src/entities/Order.java
@@ -0,0 +1,58 @@
+package entities;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    private Date moment;
+    private OrderStatus status;
+    private Client Client;
+    private List<OrdemItem> items;
+
+    //construtores
+
+
+    public Order(Date moment, List<OrdemItem> items, Client client, OrderStatus status) {
+        this.moment = moment;
+        this.items = new ArrayList<>();
+        this.Client = Client;
+        this.status = status;
+    }
+    //getters
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public List<OrdemItem> getItems() {
+        return items;
+    }
+
+    public Client getClient() {
+        return Client;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    //setters
+
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public void setItems(List<OrdemItem> items) {
+        this.items = items;
+    }
+
+    public void setClient(Client client) {
+        this.Client = Client;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+}// marcador
